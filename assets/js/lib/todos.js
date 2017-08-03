1990f3a7c0bfe2fc857f963269a65224a87ca9d5
Luc Neesby <Luc@macs-mbp.attlocal.net>
2017-08-03 11:46:35 -0700

    Added remove icon css

--- assets/js/lib/todos.js
+++ assets/js/lib/todos.js
@@ -17,7 +17,7 @@ $("input[type='text'").keypress(function(event){
 		var todoText = $(this).val();
 		$(this).val("");
 		//Create a new li and add to ul
-		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash'></span></i> " + todoText + "</li>");
 		
 	}
 });
